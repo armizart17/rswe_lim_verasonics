6aa45018aead5832fb8b825b3b84db44ae6a1fbd
armizart17 <46505393+armizart17@users.noreply.github.com>
2024-09-11 22:21:58 -0500

    wv

---
+++ utils/calc_wvlength_k.m
@@ -0,0 +1,40 @@
+function [wavelength, k] = calc_wvlength_k(sws, freq, dinf)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function [wavelength, k] = calc_wvlength_k(sws, freq, dinf)
+% Function to calculate the wavelength in pixels and the wave number
+%
+% Inputs:
+%   sws - Shear wave speed (in m/s)
+%   freq - Frequency of the wave (in Hz)
+%   dinf.dz - Axial resolution (in meters per pixel)
+%   dinf.dx - Lateral resolution (in meters per pixel)
+%
+% Outputs:
+%   wavelength - Wavelength in pixels along the axial direction
+%       wavelength.meters  [m]
+%       wavelength.pix_axi [pixels]
+%       wavelength.pix_lat [pixels]
+%   k - Wave number        [rad/m]
+% AUTHORs: Edmundo Arom Miranda 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Calculate wavelength in meters
+    wavelength.meters = sws / freq;
+    
+    % Convert wavelength to pixels
+    wavelength.pix_axi = round( wavelength.meters / dinf.dz );
+    wavelength.pix_lat = round( wavelength.meters / dinf.dx );
+
+    % Ensure wavelength_ax is an odd number
+    if mod(wavelength.pix_axi, 2) == 0
+        wavelength.pix_axi = wavelength.pix_axi + 1; % Make it odd by adding 1 if it's even
+    end
+    if mod(wavelength.pix_lat, 2) == 0
+        wavelength.pix_lat = wavelength.pix_lat + 1; % Make it odd by adding 1 if it's even
+    end
+    
+    % Calculate wave number (in radians per meter)
+    k = 2 * pi * freq / sws;
+end
